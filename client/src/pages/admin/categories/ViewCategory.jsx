2faf684c9381d8429b940dc4a3e3d3765db839a6
romasmv <rmorozovas@gmail.com>
2025-08-06 09:37:30 +0300

    update

--- client/src/pages/admin/categories/ViewCategory.jsx
+++ client/src/pages/admin/categories/ViewCategory.jsx
@@ -1,9 +1,47 @@
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
+import { Alert } from "../../../components/Alert";
 
 export function AdminViewCategoryPage() {
+    const category = {
+        title: 'Action',
+        url: 'action',
+        description: 'Very action, much movie',
+        status: 'published',
+    };
+
     return (
         <main>
             <AdminPageTitle title="View category" />
+
+            <div className="container">
+                <div className="row">
+                    <div class="col-12 col-md-9 mt-5">
+                        <Alert text='Norima kategorija nerasta, todel jos perziureti yra neimanomas.' />
+                    </div>
+                    <div class="col-12 col-md-9 mt-5">
+                        <table class="table table-bordered border-primary">
+                            <tbody>
+                                <tr class="mb-3">
+                                    <td>Title</td>
+                                    <td>{category.title}</td>
+                                </tr>
+                                <tr class="mb-3">
+                                    <td>Url slug</td>
+                                    <td>{category.url}</td>
+                                </tr>
+                                <tr class="mb-3">
+                                    <td>Description</td>
+                                    <td>{category.description}</td>
+                                </tr>
+                                <tr class="mb-3">
+                                    <td>Status</td>
+                                    <td>{category.status}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
