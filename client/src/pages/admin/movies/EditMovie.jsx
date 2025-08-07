c879f7fd8489b94572a7d06ee1e6a64809cb96f7
romasmv <rmorozovas@gmail.com>
2025-08-07 11:27:22 +0300

    up

--- client/src/pages/admin/movies/EditMovie.jsx
+++ client/src/pages/admin/movies/EditMovie.jsx
@@ -1,9 +1,32 @@
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
+import { Alert } from "../../../components/Alert";
+import { AdminMovieForm } from "../../../components/forms/AdminMovieForm";
 
 export function AdminEditMoviePage() {
+    const movie = {
+        img: '/vite.svg',
+        title: 'Action',
+        url: 'action',
+        description: 'Very action, much movie',
+        duration: 122,
+        categoryId: 1,
+        releaseDate: '2025-06-07',
+        rating: 46,
+        status: 'published',
+    };
+
     return (
         <main>
             <AdminPageTitle title="Edit movie" />
+
+            <div className="container">
+                <div className="row">
+                    <div class="col-12 col-md-9 mt-5">
+                        <Alert text='Norimas filmas nerasta, todel redagavimas yra neimanomas.' />
+                    </div>
+                    <AdminMovieForm movie={movie} />
+                </div>
+            </div>
         </main>
     );
 }
