e06e067408288d4c6f90a8d53a62cb60084842c8
romasmv <rmorozovas@gmail.com>
2025-08-13 11:46:34 +0300

    up

--- client/src/pages/public/Categories.jsx
+++ client/src/pages/public/Categories.jsx
@@ -1,7 +1,11 @@
 import { PublicPageTitle } from '../../components/PublicPageTitle';
 import { CategoryCard } from '../../components/CategoryCard';
+import { useContext } from 'react';
+import { CategoriesContext } from '../../context/categories/CategoriesContext';
 
 export function CategoriesPage() {
+    const { publicCategories } = useContext(CategoriesContext);
+
     const categoriesData = [
         {
             title: 'Action',
@@ -23,7 +27,7 @@ export function CategoriesPage() {
 
             <div className="container px-4" id="featured-3">
                 <div className="row g-4 row-cols-1 row-cols-lg-3">
-                    {categoriesData.map(category => <CategoryCard key={category.title} category={category} />)}
+                    {publicCategories.map(category => <CategoryCard key={category.title} category={category} />)}
                 </div>
             </div>
         </main>
